7dbffd600ac9dd3621b3a87710b1d8450eb9dad6
Tommy Aditya <tommyaditya0606@gmail.com>
2017-11-20 23:54:49 +0700

    User

---
+++ appngasal/api/controllers/UserController.js
@@ -0,0 +1,53 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	create: function(req, res, next){
+        User.create(req.params.all(),function userCreated(err, user){
+            if(err){
+                console.log(err)
+            
+        }
+        res.redirect('/user');
+        
+        });
+    },
+    edit: function(req, res, next){
+        User.findOne(req.param('id'), function foundUsers(err,user){
+            if(err) return next(err);
+            if(!user) return next('User doesn\'t exist.');
+            res.view({
+                user: user
+            });
+        });
+    },
+
+    update: function(req, res, next){
+        User.update(req.param('id'),req.params.all(), function userUpdated(err){
+            if(err){
+                return res.redirect('/user/' + req.param('id'));
+            }
+            res.redirect('/user');
+        });
+    },
+    delete: function(req, res, next){
+        User.findOne(req.param('id'), function foundUsers(err,user){
+            if(err) return next(err);
+
+            if(!user) return next('User doesn\'t exist.');
+
+            User.destroy(req.param('id'), function userDestroyed(err){
+                if(err) return next(err);
+            });
+            res.redirect('/user')
+        });
+    }
+       
+    
+      
+};
+
