3fd3d2f76d62b6b8fd213e6ce63a186e254a1df6
Tommy Aditya <tommyaditya0606@gmail.com>
2017-12-04 01:20:40 +0700

    Add response at user

--- appngasal/api/controllers/UserController.js
+++ appngasal/api/controllers/UserController.js
@@ -7,20 +7,29 @@
 
 module.exports = {
 	create: function(req, res, next){
-        User.create(req.params.all(),function userCreated(err, user){
-            if(err){
-                console.log(err)
-            
-        }
-        res.json(201,user);
-        //res.send('oke')
+       var email = req.param('email')
+        User.findOne({email:email}).exec(function(err, user){
+            if(!user){
+                User.create(req.params.all(),function userCreated(err, user){
+                    if(err){
+                        console.log(err)
+                    
+                }       
+                return res.send(201,'Register Succesfull')
+                
+                });
+             
+            }
+            else{
+                return res.send(400,'Email is already registered')
+            }
+        })
         
-        });
     },
     edit: function(req, res, next){
         User.findOne(req.param('id'), function foundUsers(err,user){
             if(err) return next(err);
-            if(!user) return next('User doesn\'t exist.');
+            if(!user) return res.send(404,'User doesn\'t exist.');
             // res.view({
             //     user: user
             // });
@@ -32,14 +41,14 @@ module.exports = {
             if(err){
                 return res.redirect('/user/' + req.param('id'));
             }
-            res.json(201,user);
+            res.send(200,'User has been successfully changed');
         });
     },
     delete: function(req, res, next){
         User.findOne(req.param('id'), function foundUsers(err,user){
             if(err) return next(err);
 
-            if(!user) return next('User doesn\'t exist.');
+            if(!user) return res.send(404,'User doesn\'t exist.');
 
             User.destroy(req.param('id'), function userDestroyed(err){
                 if(err) return next(err);
